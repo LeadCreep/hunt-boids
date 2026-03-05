5afd3ecd3fd8d487c4dfdd3bc864d0ef2aad86f3
DELCROIX Victor <37382189+LeadCreep@users.noreply.github.com>
2026-03-05 20:25:00 +0100

    Day Night Cycle

---
+++ scripts/day_night.gd
@@ -0,0 +1,49 @@
+extends Node
+
+@export var sunlight: DirectionalLight3D
+@export var world_environment: WorldEnvironment
+@export var day_duration: float = 20.0
+@export var debug_label: Label
+
+@export var time_of_day: float = 0.0
+
+func _ready() -> void:
+	sunlight.rotation_degrees.x = time_of_day * 360.0
+	_update_environment()
+
+
+func _process(delta: float) -> void:
+	time_of_day += (delta / day_duration)
+
+	if time_of_day >= 1.0:
+		time_of_day -= 1.0
+	
+	sunlight.rotation_degrees.x = time_of_day * 360.0
+	_update_environment()
+	debug_label.text = get_clock_time()
+
+
+func _update_environment() -> void:
+	var deg_x: float = sunlight.rotation_degrees.x
+
+	if is_daytime(deg_x):
+		var intensity = _sun_intensity(deg_x)
+		sunlight.light_energy = intensity
+	else:
+		sunlight.light_energy = 0.0
+
+func is_daytime(deg_x: float) -> bool:
+	return deg_x > 90.0 and deg_x < 270.0
+
+func is_nighttime(deg_x: float) -> bool:
+	return not is_daytime(deg_x)
+
+func _sun_intensity(deg_x: float) -> float:
+	var normalized = (deg_x - 90.0) / 180.0
+	return sin(normalized * PI)
+
+func get_clock_time() -> String:
+	var total_minutes: int = int(time_of_day * 24.0 * 60.0)
+	var hours: int = total_minutes / 60
+	var minutes: int = total_minutes % 60
+	return "%02d:%02d" % [hours, minutes]
